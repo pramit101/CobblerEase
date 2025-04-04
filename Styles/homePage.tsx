e6bb6839e62d3e1c808c51c14a8a7d1619af4a11
Pramit Gautam <gautampramit726@gmail.com>
2025-04-04 11:42:10 +1100

    made some style changes

---
+++ Styles/homePage.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  heading: {
+    backgroundColor: "#f0f036",
+    justifyContent: "center",
+    width: "100%",
+    height: 120,
+    padding: 20,
+  },
+  heading_text: {
+    fontSize: 30,
+    fontWeight: "900",
+    color: "black",
+    textAlign: "center",
+    margin: 10,
+  },
+  sub_heading: {
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: 50,
+  },
+  sub_heading_text: {
+    fontSize: 20,
+    fontWeight: "500",
+    color: "white",
+    textAlign: "center",
+    margin: 10,
+  },
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,25 @@
 import { Image, StyleSheet, Platform, View, Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "../../Styles/homePage";
 
 export default function HomeScreen() {
   return (
-    <View className="main" style={styles.main}>
-      <Text className="initial" style={styles.initial}>
-        Hello World!
-      </Text>
-      <Text className="initial" style={styles.initial}>
-        Pramit 21951900.
-      </Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View className="main" style={styles.main}>
+          <View className="heading" style={styles.heading}>
+            <Text className="heading_text" style={styles.heading_text}>
+              CobblerEase
+            </Text>
+          </View>
+          <View className="sub_heading" style={styles.sub_heading}>
+            <Text className="sub_heading_text" style={styles.sub_heading_text}>
+              Home
+            </Text>
+          </View>
+          <Text>Pramit 21951900.</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5",
-  },
-  initial: {
-    fontSize: 20,
-    color: "green",
-    fontWeight: "bold",
-    textAlign: "center",
-    margin: 10,
-  },
-});
