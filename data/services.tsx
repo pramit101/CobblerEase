54883475e63cd7f26c7e908dbdfdad29fcbf9437
Pramit Gautam <gautampramit726@gmail.com>
2025-04-18 09:44:03 +1000

    added service data

---
+++ data/services.tsx
@@ -0,0 +1,9 @@
+export const services = [
+  {
+    service_name: "Shoe Polish",
+    service_id: "12345",
+    service_description:
+      "Restores shine and color while protecting your shoes from wear and tear.",
+    service_image: require("../assets/images/shoe_polish.jpeg"),
+  },
+];
